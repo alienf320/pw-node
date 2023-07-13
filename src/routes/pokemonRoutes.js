1fa6b7cba27081d0e848c6f5137677d2eec21f31
Martín Chavez <cmchavez@applaudostudios..com>
2023-07-13 18:36:52 -0300

    pokemonRoutes: router.get('/full...) now search for partial matches

--- src/routes/pokemonRoutes.js
+++ src/routes/pokemonRoutes.js
@@ -24,16 +24,27 @@ router.get('/', async (req, res) => {
 router.get('/full', async (req, res) => {
 
   try {
-    const pk = await Pokemon.find(req.query).populate('moves.move');
-    if(!pk) {
-      console.log("Que hacemo?")
-      return res.status(404).send({message: "No encontramos nada"})
+    const query = req.query;
+    const regexQuery = {};
+
+    // Convertir cada valor de la consulta en una expresión regular
+    Object.keys(query).forEach(key => {
+      regexQuery[key] = new RegExp(query[key], 'i'); // 'i' para hacer la búsqueda insensible a mayúsculas y minúsculas
+    });
+
+    const pk = await Pokemon.find(regexQuery).populate('moves.move');
+
+    if (pk.length === 0) {
+      console.log("¿Qué hacemos?")
+      return res.status(404).send({message: "No encontramos nada"});
     }
-    res.send(pk)
+
+    res.send(pk);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
     
 });
+
 
 module.exports = router
